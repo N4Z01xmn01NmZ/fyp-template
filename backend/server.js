28d2ce5a8469a6e13396ef8957f21221e716af10
N4Z01xmn01NmZ <nazoixmnoinmz@gmail.com>
2021-08-31 22:27:50 +0700

    Added material overview "machine programming"

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,43 @@ const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 
 io.on('connection', socket => {
+    console.log(`${socket.id} connected`);
 
+    socket.on('checkin', () => {
+
+    });
+
+    socket.on('checkout', () => {
+
+    });
+
+    socket.on('host_room', () => {
+
+    });
+
+    socket.on('close_room', () => {
+
+    });
+
+    socket.on('leave_room', () => {
+
+    });
+
+    socket.on('join_room', () => {
+
+    });
+
+    socket.on('message', () => {
+
+    });
+
+    socket.on('announcement', () => {
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected`);
+    });
 });
 
 const PORT = process.env.PORT || 4433;
